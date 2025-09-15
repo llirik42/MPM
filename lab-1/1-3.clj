bc7b6067b331ff0beea7d9cc4c414c61ea31172c
Kirill Kondrenko <kondrenkokp@yandex.ru>
2025-09-15 10:07:46 +0700

    Implemented my-map and my-filter

--- lab-1/1-3.clj
+++ lab-1/1-3.clj
@@ -7,7 +7,19 @@
             acc))]
   
   (letfn [(my-map [f coll] (_my-map f coll `()))]
-    
-    (println (my-map dec `(1 2 3 4 5)))
-    (println (my-map inc `(1)))
-    (println (my-map inc ()))))
+
+    (letfn [(_my-filter
+              [pred coll acc]
+              (if (> (count coll) 0)
+                (if (pred (first coll))
+                  (recur pred (rest coll) (concat acc (list (first coll))))
+                  (recur pred (rest coll) acc))
+                acc))]
+
+      (letfn [(my-filter
+                [pred coll]
+                (_my-filter pred coll `()))]
+        (println (my-map dec `(1 2 3 4 5)))
+        (println (my-map inc `(1)))
+        (println (my-map inc `()))
+        (println (my-filter even? `(8 1 2 3 4 5 6)))))))
