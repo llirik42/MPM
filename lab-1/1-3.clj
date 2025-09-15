7537673b169231b0fa218a25b390af82c5630631
Kirill Kondrenko <kondrenkokp@yandex.ru>
2025-09-15 10:42:30 +0700

    Implemented my-map and my-filter by reduce. Deleted unused code

--- lab-1/1-3.clj
+++ lab-1/1-3.clj
@@ -7,18 +7,18 @@
            `()
            coll))]
 
-  (letfn [(_my-filter
-            [pred coll acc]
-            (if (> (count coll) 0)
-              (if (pred (first coll))
-                (recur pred (rest coll) (concat acc (list (first coll))))
-                (recur pred (rest coll) acc))
-              acc))]
-
-    (letfn [(my-filter
-              [pred coll]
-              (_my-filter pred coll `()))]
-      (println (my-map dec `(1 2 3 4 5)))
-      (println (my-map inc `(1)))
-      (println (my-map inc `()))
-      (println (my-filter even? `(8 1 2 3 4 5 6))))))
+  (letfn [(my-filter
+            [pred coll]
+            (reduce
+             (fn [acc el]
+               (if (pred el)
+                 (concat acc (list el))
+                 acc))
+             `()
+             coll))]
+    (println (my-map dec `(1 2 3 4 5)))
+    (println (my-map inc `(1)))
+    (println (my-map inc `()))
+    (println (my-filter even? `(8 1 2 3 4 5 6)))
+    (println (my-filter odd? `(8 1 2 3 4 5 6)))
+    (println (my-filter odd? `()))))
