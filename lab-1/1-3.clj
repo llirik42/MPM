dc4bc6dca9f778bca4a7961c3b3edfbdd5ef07b3
Kirill Kondrenko <kondrenkokp@yandex.ru>
2025-09-15 10:34:24 +0700

    Implemented my-map by reduce

--- lab-1/1-3.clj
+++ lab-1/1-3.clj
@@ -1,25 +1,24 @@
-(letfn [(_my-map
-          [f coll acc]
-          (if (> (count coll) 0)
-            (let [acc-delta (f (first coll))
-                  new-acc (concat acc (list acc-delta))]
-              (recur f (rest coll) new-acc))
-            acc))]
-  
-  (letfn [(my-map [f coll] (_my-map f coll `()))]
+(letfn [(my-map
+          [f coll]
+          (reduce
+           (fn [acc el]
+             (let [acc-delta (f el)]
+               (concat acc (list acc-delta))))
+           `()
+           coll))]
 
-    (letfn [(_my-filter
-              [pred coll acc]
-              (if (> (count coll) 0)
-                (if (pred (first coll))
-                  (recur pred (rest coll) (concat acc (list (first coll))))
-                  (recur pred (rest coll) acc))
-                acc))]
+  (letfn [(_my-filter
+            [pred coll acc]
+            (if (> (count coll) 0)
+              (if (pred (first coll))
+                (recur pred (rest coll) (concat acc (list (first coll))))
+                (recur pred (rest coll) acc))
+              acc))]
 
-      (letfn [(my-filter
-                [pred coll]
-                (_my-filter pred coll `()))]
-        (println (my-map dec `(1 2 3 4 5)))
-        (println (my-map inc `(1)))
-        (println (my-map inc `()))
-        (println (my-filter even? `(8 1 2 3 4 5 6)))))))
+    (letfn [(my-filter
+              [pred coll]
+              (_my-filter pred coll `()))]
+      (println (my-map dec `(1 2 3 4 5)))
+      (println (my-map inc `(1)))
+      (println (my-map inc `()))
+      (println (my-filter even? `(8 1 2 3 4 5 6))))))
