496ea5c72156d20c06a7e321325cf64b2f2e99f2
Kirill Kondrenko <kondrenkokp@yandex.ru>
2025-09-30 18:22:21 +0700

    Some progress

--- lab-2/2-0.clj
+++ lab-2/2-0.clj
@@ -1,26 +1,30 @@
-(defn my-func [x] x)
+(ns lab_2/2_0
+  (:require [incanter.charts :as charts]
+            [incanter.core :as ic]))
+(def h 0.01)
 
-(defn integral [f]
-  (let [h 0.1]
-    (letfn [(_int-k [k acc]
-                    (if (> k 0)
-                      (let [f-prev (f (* h (- k 1)))
-                            f-cur (f (* h k))
-                            coeff (/ h 2)
-                            delta (* coeff (+ f-prev f-cur))]
-                        (recur (- k 1) (+ acc delta)))
-                      acc))
-            (_int-x [x]
-                    (let [k (Math/round (/ x h))]
-                      (_int-k k 0)))
-            (_int-x-memo
-             (memoize _int-x))]
-      _int-x-memo)))
+(defn f [x] 1)
 
+(defn integral-k
+  [k acc]
+  (if (> k 0)
+    (let [f-prev (f (* h (- k 1)))
+          f-cur (f (* h k))
+          coeff (/ h 2)
+          delta (* coeff (+ f-prev f-cur))]
+      (recur (- k 1) (+ acc delta)))
+    acc))
 
-(time ((integral my-func) 10000000))
-(time ((integral my-func) 10000000))
-(time ((integral my-func) 10000000))
-(time ((integral my-func) 10000000))
+(def integral-k-memo (memoize integral-k))
+
+(defn integral
+  [x]
+  (let [k (Math/round (/ x h))]
+    (integral-k-memo k 0)))
+
+;; (println ((integral my-func) 10000000))
+(println (integral 1000000))
+(time (integral 1000000))
+(time (integral 1000000))
 
 ;; (println (Math/round (/ 10 0.12)))
