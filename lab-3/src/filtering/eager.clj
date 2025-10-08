51a164c323179788d42dbed583e7c29145b2ceba
Kirill Kondrenko <kondrenkokp@yandex.ru>
2025-10-08 23:57:08 +0700

    Fixed lazy pfilter

--- lab-3/src/filtering/eager.clj
+++ lab-3/src/filtering/eager.clj
@@ -12,23 +12,29 @@ partition
   (lazy-seq
    (cons 1 (map inc naturals))))
 
+pmap
+
 (defn pfilter
   ([pred partitions]
    (lazy-seq
-   (when-let [s (seq (take 8 partitions))]
-     (concat (->> s
-          (map #(future (filter pred %)))
-          (doall)
-          (map deref)
-          (doall)
-          (reduce concat)) (pfilter pred (drop 8 partitions))))))
+    (when-let [s (seq (take 8 partitions))]
+      (concat (->> s
+                   (map #(future (doall (filter pred %))))
+                   (doall)
+                   (map deref)
+                   (doall)
+                   (reduce concat)) (pfilter pred (drop 8 partitions))))))
   ([pred coll block-size] (pfilter pred (-partition block-size coll))))
 
-(time (pfilter even? naturals 32))
-(time (nth (pfilter even? naturals 32) 1000000))
+(defn my-pred [_]
+  (Thread/sleep 100)
+  true)
 
-;; (println (nth (-partition 32 naturals) 0))
-;; (println (nth (-partition 32 naturals) 1))
-;; (println (nth (-partition 32 naturals) 2))
-;; (println (nth (-partition 32 naturals) 3))
-;; (println (nth (-partition 32 naturals) 4))
+(time (doall (pfilter my-pred (range 16) 1)))
+(time (doall (filter my-pred (range 16))))
+
+
+
+
+;; (time (pfilter even? naturals 32))
+;; (time (nth (pfilter even? naturals 32) 1000000))
