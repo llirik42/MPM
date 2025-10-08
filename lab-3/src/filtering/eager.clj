e059b286a7e51c07610ee95c0474797d4d21e04a
Kirill Kondrenko <kondrenkokp@yandex.ru>
2025-10-08 23:37:21 +0700

    Implemented lazy pfilter

--- lab-3/src/filtering/eager.clj
+++ lab-3/src/filtering/eager.clj
@@ -3,23 +3,32 @@
 partition
 
 (defn -partition
-  ([n coll]
-   (lazy-seq
-    (when-let [_ (seq coll)]
-      (cons (take n coll) (-partition n (drop n coll)))))))
+  [n coll]
+  (lazy-seq
+   (when-let [s (seq coll)]
+     (cons (take n s) (-partition n (drop n s))))))
 
 (def naturals
   (lazy-seq
-   (cons 1 (map inc naturals)))) 
+   (cons 1 (map inc naturals))))
 
-;; (defn pfilter
-;;   ([pred coll block-size]
-;;    ())
-;;   ([pred coll] (pfilter pred coll 32)))
+(defn pfilter
+  ([pred partitions]
+   (lazy-seq
+   (when-let [s (seq (take 8 partitions))]
+     (concat (->> s
+          (map #(future (filter pred %)))
+          (doall)
+          (map deref)
+          (doall)
+          (reduce concat)) (pfilter pred (drop 8 partitions))))))
+  ([pred coll block-size] (pfilter pred (-partition block-size coll))))
 
+(time (pfilter even? naturals 32))
+(time (nth (pfilter even? naturals 32) 1000000))
 
-(println (nth (-partition 32 naturals) 0))
-(println (nth (-partition 32 naturals) 1))
-(println (nth (-partition 32 naturals) 2))
-(println (nth (-partition 32 naturals) 3))
-(println (nth (-partition 32 naturals) 4))
+;; (println (nth (-partition 32 naturals) 0))
+;; (println (nth (-partition 32 naturals) 1))
+;; (println (nth (-partition 32 naturals) 2))
+;; (println (nth (-partition 32 naturals) 3))
+;; (println (nth (-partition 32 naturals) 4))
--- lab-3/src/filtering/lazy.clj
+++ lab-3/src/filtering/lazy.clj
@@ -1,3 +1,6 @@
 (ns filtering.lazy)
 
 (defn my-filter [pred coll] (filter pred coll))
+
+(println (when-let [_ (seq (take 5 `()))]
+  5))
