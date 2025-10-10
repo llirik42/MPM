7380f770a82b52a80a5b2fe7f27ee43443089663
Kirill Kondrenko <kondrenkokp@yandex.ru>
2025-10-10 21:04:18 +0700

    Implemented core logic of 4th lab

---
+++ lab-4/src/dnf/core.clj
@@ -0,0 +1,70 @@
+(ns dnf.core)
+
+(defn -integer-to-boolean
+  [i]
+  {:pre [(int? i)]}
+  (if (== i 0) false true))
+
+(defn const
+  [value]
+  {:pre [(or (boolean? value) (int? value))]}
+  (let [v (if (int? value)
+            (-integer-to-boolean value)
+            value)]
+    (list ::const v)))
+
+(defn const?
+  [expr]
+  (= (first expr) ::const))
+
+(defn const-value
+  [c]
+  (second c))
+
+(defn variable
+  [name]
+  {:pre [(keyword? name)]}
+  (list ::variable name))
+
+(defn variable?
+  [expr]
+  (= (first expr) ::variable))
+
+(defn variable-name
+  [v]
+  (second v))
+
+(defn lneg
+  [expr]
+  (list ::neg expr))
+
+(defn lneg?
+  [expr]
+  (= (first expr) ::neg))
+
+(defn land
+  [expr & other]
+  (cons ::and (cons expr other)))
+
+(defn land?
+  [expr]
+  (= (first expr) ::and))
+
+(defn lor
+  [expr & other]
+  (cons ::or (cons expr other)))
+
+(defn lor?
+  [expr]
+  (= (first expr) ::or))
+
+(defn limpl
+  [e1 e2]
+  (list ::impl e1 e2))
+
+(defn limpl?
+  [expr]
+  (= (first expr) ::impl))
+
+(defn args [expr]
+  (rest expr))
