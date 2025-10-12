23630702744e4ae1bcb719df6f7d1f69eb619983
Kirill Kondrenko <kondrenkokp@yandex.ru>
2025-10-12 22:27:56 +0700

    Хм

--- lab-4/src/logic/dnf.clj
+++ lab-4/src/logic/dnf.clj
@@ -34,45 +34,52 @@
         (const (not v))))] ; вычисления значения новой константы 
    [(fn [expr] (and (lneg? expr) (variable? (first-arg expr)))); Отрицание переменной 
     (fn [expr] expr)] ; возвращаем без изменений
+   
    [(fn [expr] (and (lneg? expr) (lneg? (first-arg expr)))); Отрицание отрицания 
     (fn [expr] (let [neg-arg (first-arg (first-arg expr))]
                  (dnf neg-arg)))] ; Возврат выражения без отрицания (Двойное отрицание)
+   
+   ; TODO: пофиксить (dnf не там, где надо)
    [(fn [expr] (and (lneg? expr) (land? (first-arg expr)))); Отрицание И (закон Де-моргана)
     (fn [expr] (let [and-args (args (first-arg expr))
                      mapped-and-args (map #(dnf (lneg %)) and-args)]
                  (dnf (apply lor mapped-and-args))))] ; Закон Де-моргана
+   
+   ; TODO: пофиксить (dnf не там, где надо)
    [(fn [expr] (and (lneg? expr) (lor? (first-arg expr)))); Отрицание ИЛИ (закон Де-моргана)
     (fn [expr] (let [or-args (args (first-arg expr))
                      mapped-or-args (map #(dnf (lneg %)) or-args)]
                  (dnf (apply land mapped-or-args))))] ; Закон Де-моргана
+   
+   ; TODO: пофиксить (dnf не там, где надо)
    [(fn [expr] (and (lneg? expr) (limpl? (first-arg expr)))); Отрицание импликации
     (fn [expr] (let [arg (first-arg expr)] ; arg is implication
                  (dnf (lneg (dnf arg)))))]
-
+   
    [(fn [expr] (and (land? expr) (== 1 (count (args expr))))) ; Вырожденный случай: конъюнкция одного аргумента
     (fn [expr] (dnf (first-arg expr)))]
-
+   
    [(fn [expr] (and (== 2 (count (args expr))) (land? expr) (lor? (second-arg expr)))); A & (B v C v D v ...)
     (fn [expr] (let [arg1 (first-arg expr)
                      arg2 (second-arg expr)
                      or-args (args arg2)
-                     ands (map #(land arg1 %) or-args)]
+                     ands (map #(land arg1 %) or-args)] ; TODO: тут нет dnf
                  (apply lor ands)))]
 
    [(fn [expr] (and (== 2 (count (args expr))) (land? expr) (lor? (first-arg expr)))); (B v C v D v ...) & A
     (fn [expr] (let [arg1 (first-arg expr)
                      arg2 (second-arg expr)
                      or-args (args arg1)
-                     ands (map #(land % arg2) or-args)]
-                 (apply lor ands)))]
-
+                     ands (map #(land % arg2) or-args)] ; TODO: тут нет dnf
+                 (apply lor ands)))] 
+   
    [land? ; все остальные случаи для конъюнкции
     (fn [expr] (if (> (count (args expr)) 2)
                  (let [args (args expr)
                        f (first args)
                        r (rest args)
                        tmp (land (dnf f) (dnf (apply land r)))] ; TODO: назвать нормально 
-                   (flatten-and tmp))
+                   (dnf (flatten-and tmp)))
                  (land (dnf (first-arg expr)) (dnf (second-arg expr)))))] ; точно 2 аргумента
 
    [(fn [expr] (and (lor? expr) (== 1 (count (args expr))))) ; Вырожденный случай: дизъюнкция одного аргумента
@@ -99,7 +106,9 @@
       b (variable ::B)
       c (variable ::C)
       d (variable ::D)
-      v (land a b c d)]
-  (println (repr v))
+      v (land a (lor b c) d)
+      v2 (land a (land b c))]
+  ; НЕ РАБОТАЕТ
+  (println (repr v2))
   (println "----")
-  (println (repr (dnf v))))
+  (println (repr (dnf v2))))
